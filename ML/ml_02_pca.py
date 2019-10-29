4d124ce98b4f6712cbfc8be3a75a9596f88488e9
fa97ber <fberkowitz@web.de>
2019-10-29 12:10:11 +0100

    Add files via upload

---
+++ ML/ml_02_pca.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+def pca(df, r):
+    cols = df.columns
+    
+    #Zentrieren
+    for col in cols:
+        df[col] = df[col] - np.mean(df[col])
+        
+    #Normieren der Varianz
+    for col in cols:
+        df[col] = df[col] / np.std(df[col])
+    
+    #In Matrix umwandeln
+    n, d = np.shape(df)
+    X = np.zeros((n, d))
+    for ni in range(n):
+        for di in range(d):
+            X[ni, di] = df[cols[di]][ni]
+    
+    #Singulärwertzerlegung
+    u,d,vt = np.linalg.svd(X, full_matrices=False)
+    v = vt.transpose()
+    
+    ud = u * d
+    pc = []
+    ai = []
+    
+    for ri in range(r):
+        pc.append(v[:,ri])
+        ai.append(ud[:,ri])
+        
+    return pc,ai,np.power(d, 2)/(n-1)
